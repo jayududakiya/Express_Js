617f8ec1bb24adc1c014c9054f2bad0e76dc48ef
JD <137084651+jayududakiya@users.noreply.github.com>
2024-08-11 15:19:58 +0530

    Add API of user and product database : [PUT [reples] , PATCH [update] , DELETE ] with fs module

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-/* // TODO ---------------------------------- lesson 05 [09/08/2024]  ---------------------------------- */
-// !CREATE [API] FOR PRODUCTS -> GET [read] , GET/:ID [find/read] , POST [create]
+
+/* // TODO ---------------------------------- lesson 06 [10/08/2024]  ---------------------------------- */
+
+// !CREATE [API] FOR PRODUCTS -> GET [read] , GET/:ID [find/read] , POST [create] , PUT [reples] , PATCH [update] , DELETE 
 
 const express = require("express")
 const morgan = require("morgan")
 const fs = require('fs')
-const path = require('path');
-
 
 //? CREATE APP
 const app  = express();
 app.use( express.json())
 app.use(morgan("dev"))
+
 
 // IMPORT DATA 
 // const products = require("./Data/products.json")
@@ -51,7 +52,46 @@ app.post("/products",(req,res)=>{
     }
 })
 
-// !CREATE [API] FOR USERDATA -> GET [read] , GET/:ID [find/read] , POST [create]
+//** REPLASE PRODUCTS 
+app.put("/products/:id",(req,res)=>{
+    let ID = +req.params.id
+    const productIndex = products.findIndex(product => product.id === ID)
+    if(productIndex !== -1){
+        products[productIndex] = {id : ID , ...req.body}
+        fs.writeFileSync('./Data/products.json' , JSON.stringify(products , null , 4) ,'utf-8')
+        res.json({message : "Products Was Reples SuccesFuly " , product : products[productIndex]})
+    }else{
+        res.json({message : "Products was NOT Replase Try Again"})
+    }
+})
+
+//** UPADTE PRODUCTS 
+app.patch("/products/:id",(req,res)=>{
+    let ID = +req.params.id
+    const productIndex = products.findIndex(product => product.id === ID)
+    if(productIndex !== -1){
+        products[productIndex] = {...products[productIndex],...req.body}
+        fs.writeFileSync('./Data/products.json' , JSON.stringify(products , null , 4) ,'utf-8')
+        res.json({message : "Products Was Upadte SuccesFuly " , product : products[productIndex]})
+    }else{
+        res.json({message : "Products was NOT Upadte Try Again"})
+    }
+})
+
+// ** DELETE PRODUCTS DATA 
+app.delete("/products/:id",(req,res)=>{
+    const ID = +req.params.id;
+    const index = products.findIndex(product => product.id === ID)
+    const restProducts = products.filter(product => product.id !== ID)
+    if(index !== -1){
+        fs.writeFileSync('./Data/products.json',JSON.stringify(restProducts , null , 4) , 'utf-8')
+        res.json({message : "Product Was Delete SuccessFully",product : products[index]})
+    }else{
+        res.json({message : "Product Was NOT Delete"})
+    }
+})
+
+// !CREATE [API] FOR USERDATA -> GET [read] , GET/:ID [find/read] , POST [create] , PUT [reples] , PATCH [update] , DELETE
 
 //IMPORT ALL USER DATA with fs module 
 const userData = JSON.parse(fs.readFileSync('./Data/userData.json' , 'utf-8'))
@@ -74,7 +114,6 @@ app.get('/user/:id',(req,res)=>{
 })
 
 //** CREATE new USER
-
 app.post('/user',(req,res)=>{
     const BODY = req.body
     if(Boolean(Object.keys(BODY).length)){
@@ -85,7 +124,59 @@ app.post('/user',(req,res)=>{
         res.json({message : "User was NOT Add Try Again for Validate Schema.."})
     }
 })
+ 
+//** REPLASE USER
 
+app.put('/user/:id' , (req,res)=>{
+    const ID = +req.params.id;
+    const userIndex = userData.findIndex(user => user.id === ID)
+    if(userIndex !== -1){
+        //  * WAHY 01 TO REAPLSE DATA 
+        // userData[userIndex] = {id : ID , ...req.body};
+        //  * WAHY 02 TO REAPLSE DATA 
+        userData.splice(userIndex , 1,{id : ID , ...req.body})
+        fs.writeFileSync('./Data/userData.json',JSON.stringify(userData , null , 2) , 'utf-8');
+        res.json({message:"user Data replase SuccessFully" , user : req.body})
+    }else{
+        res.json({message:"user Data Not replase Try Again"})
+    }
+})
+
+//** UPDATE USER
+app.patch('/user/:id' , (req,res)=>{
+    const ID = +req.params.id;
+    const userIndex = userData.findIndex(user => user.id === ID)
+    if(userIndex !== -1){
+        //  * WAHY 01 TO UPDATE DATA 
+        // userData[userIndex] = {...userData[userIndex] , ...req.body};
+        //  * WAHY 02 TO REAPLSE DATA 
+        userData.splice(userIndex , 1,{...userData[userIndex], ...req.body})
+        fs.writeFileSync('./Data/userData.json',JSON.stringify(userData , null , 2) , 'utf-8');
+        res.json({message:"user Data replase SuccessFully" , user : req.body})
+    }else{
+        res.json({message:"user Data Not replase Try Again"})
+    }
+})
+
+// ** DELETE USER 
+app.delete('/user/:id' , (req,res)=>{
+    const ID  = +req.params.id;
+    const index = userData.findIndex(user => user.id === ID);
+    //* WAHY 01 TO DELETE DATA 
+    // const restUser = userData.filter(user => user.id !== ID);
+    //* WAHY 02 TO DELETE DATA 
+    const user = userData[index]
+    userData.splice(index,1);
+    if(index !== -1){
+        //* use with WAHY 01 TO DELETE DATA 
+        // fs.writeFileSync('./Data/userData.json',JSON.stringify(restUser,null,2),"utf-8");
+        //* use with WAHY 02 TO DELETE DATA 
+        fs.writeFileSync('./Data/userData.json',JSON.stringify(userData,null,2),"utf-8");
+        res.json({message : "user Was Delete SuccessFully",user})
+    }else{
+        res.json({message : "user Was Not Deleter"})
+    }
+})
 
 app.listen(1200,()=>{
     console.log("Server Is Start At Port 1200");
